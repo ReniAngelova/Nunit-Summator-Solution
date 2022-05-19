d6072e907f29901f10ed3003aee36e839c607b9c
ReniAngelova <105847607+ReniAngelova@users.noreply.github.com>
2022-05-19 11:09:40 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Summator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+
+            if (sum == 21)
+            {
+                Console.WriteLine("Test Pass");
+            }
+            else
+            {
+                Console.WriteLine("Test Fail");
+            }
+
+            long average = Summator.Average(new int[] { 5, 10, 15 });
+            if (average == 10)
+            {
+                Console.WriteLine("Test Pass");
+            }
+            else
+            {
+                Console.WriteLine("Test Fail");
+            }
+        }
+    }
+}
